65081d1e67fa9bdcc590a1b6d7dd90e1c985bcd8
PedroGodri <pedro.g2006@aluno.ifsc.edu.br>
2022-10-18 12:39:31 -0300

    Commit pprojeto animal com scanner

---
+++ ProjetoAnimalScanner/src/classes/Animal.java
@@ -0,0 +1,62 @@
+package classes;
+
+public class Animal {
+	private String nome;
+	private String  raca;
+	private Float comprimento;
+	private Integer numeroPatas;
+	private String cor;
+	private String ecoSistema;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRaca() {
+		return raca;
+	}
+
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+
+	public Float getComprimento() {
+		return comprimento;
+	}
+
+	public void setComprimento(Float comprimento) {
+		this.comprimento = comprimento;
+	}
+
+	public Integer getNumeroPatas() {
+		return numeroPatas;
+	}
+
+	public void setNumeroPatas(Integer numeroPatas) {
+		this.numeroPatas = numeroPatas;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getEcoSistema() {
+		return ecoSistema;
+	}
+
+	public void setEcoSistema(String ecoSistema) {
+		this.ecoSistema = ecoSistema;
+	}
+
+	public void caminhar() {
+		
+	}
+}
